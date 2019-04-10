ae4e401971897c634d786a51f10a2426691353d0
igsnow <zzy716810918@gmail.com>
2019-04-11 00:42:11 +0800

    布局导航

---
+++ examples/components/navi-card/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="navi-card">
+        <image class="navi-icon" :src="icon"/>
+        <span>{{title}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        props: {
+            icon: {
+                type: String,
+                default: ''
+            },
+            title: {
+                type: String,
+                default: ''
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .navi-card
+        width: 175px
+        height: 120px
+        background: rgba(255, 255, 255, 1)
+        box-shadow: 0 4px 10px 0 rgba(9, 36, 66, 0.04)
+        border-radius: 4px
+        display: flex
+        flex-direction: column
+        justify-content: center
+        align-items: center
+        font-size: 14px
+        font-weight: 400
+        color: rgba(89, 108, 142, 1)
+        line-height: 20px
+        margin-top: 15px
+
+        .navi-icon
+            width: 33px
+            height: 33px
+            margin-bottom: 17px
+</style>
--- examples/pages/index/index.vue
+++ examples/pages/index/index.vue
@@ -1,19 +1,38 @@
 <template>
     <div class="container">
         <div class="header">
-            <span class="header-desc">Blaze UI</span>
+            <image class="header-bg" src="/images/static/index_bg.png"></image>
+            <span class="header-desc"><span class="header-name">Blaze UI</span>移动端组件库</span>
         </div>
-        <div class="main"></div>
+        <div class="main">
+            <navi-card
+                    v-for="(item,index) in naviConfigs.baseConfig"
+                    :key="index"
+                    :icon="item.icon"
+                    :title="item.title"
+            >
+            </navi-card>
+        </div>
     </div>
 </template>
 
 <script>
+    import naviCard from '@/components/navi-card/index'
+    import naviConfigs from '@/config/navi'
+
     export default {
-        name: "index"
+        name: "index",
+        data() {
+            return {
+                naviConfigs: naviConfigs
+            }
+        },
+        components: {
+            naviCard
+        }
     }
 </script>
 
 <style lang="stylus" scoped>
-    .title
-        height: 40px;
+
 </style>
